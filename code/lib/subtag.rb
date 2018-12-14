7bb98c692adf6c9cccc0608ab2f71c2aeb31bc74
Arthur Reutenauer <arthur.reutenauer@klarna.com>
2018-12-14 23:59:09 +0100

    WIP Registry.parse

--- code/lib/subtag.rb
+++ code/lib/subtag.rb
@@ -1,5 +1,42 @@
+require 'date'
+
 class Registry
+  attr_accessor :file_date, :subtags
+
+  def initialize
+    @subtags = []
+  end
+
+  def self.parse
+    registry = new
+    subtag = Subtag.new
+    File.read(File.expand_path('../../language-subtag-registry', __dir__)).each_line do |line|
+      if line =~ /^File-Date: (.*)$/
+        puts 1
+        registry.file_date = Date.parse($1)
+      elsif line =~ /%%/
+        puts 2
+        registry.add_subtag subtag
+        puts registry.subtags.count
+        subtag = Subtag.new
+      end
+    end
+
+    registry
+  end
+
+  def add_subtag(subtag)
+    @subtags << subtag
+  end
 end
 
 class Subtag
+  def initialize(params = { })
+    @code = params[:code]
+    @type = params[:type]
+    @scope = params[:scope]
+    @added = params[:added]
+    @suppress_script = params[:suppress_script]
+    @descriptions = params[:descrptions]
+  end
 end
--- code/spec/subtag_spec.rb
+++ code/spec/subtag_spec.rb
@@ -1,18 +1,61 @@
 require 'spec_helper'
 
 describe Registry do
-  it "has a file date"
+  let(:registry) { Registry.new }
 
-  describe "parse" do
-    it "returns an array of subtags"
+  it "has a file date" do
+    expect(Registry.parse.file_date).to eq Date.new(2018, 11, 30)
+  end
+
+  it "has subtags" do
+    expect(registry.subtags).to be_an Array
+  end
+
+  describe '.parse' do
+    it "returns a registry" do
+      expect(Registry.parse).to be_a Registry
+    end
+
+    it "returns subtags" do
+      expect(Registry.parse.subtags.map(&:class).uniq).to eq [Subtag]
+    end
+  end
+
+  describe '#add_subtag' do
+    it "adds a subtag to the registry" do
+      expect do
+        registry.add_subtag Subtag.new(code: 'hi', descriptions: ["Hindi"])
+      end.to change(registry.subtags, :count).by(1)
+    end
   end
 end
 
 describe Subtag do
+  let(:subtag) do
+    Subtag.new(
+      type: "language",
+      code: "hrx",
+      descriptions: ["Hunsrik"],
+      added: Date.new(2009, 7, 29),
+    )
+  end
+
   it "has a type"
   it "has a code"
   it "has descriptions"
   it "has an added date"
   it "has a suppress-script"
   it "has a scope"
+
+  describe '.new' do
+    it "returns a Subtag" do
+      expect(Subtag.new).to be_a Subtag
+    end
+
+    it "takes an optional parameter hash" do
+      expect do
+        subtag = Subtag.new(code: "hrz", descriptions: "Harzani")
+      end.not_to raise_exception
+    end
+  end
 end
